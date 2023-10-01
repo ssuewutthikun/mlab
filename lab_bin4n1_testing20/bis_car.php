f94c6c6b6d0ca621bb186223c055aaf4d6955f2c
ssuewutthikun <31299899+ssuewutthikun@users.noreply.github.com>
2023-10-01 10:51:18 +0700

    Add files via upload

--- lab_bin4n1_testing20/bis_car.php
+++ lab_bin4n1_testing20/bis_car.php
@@ -5,13 +5,35 @@
     <title>index</title>
 </head>
 <body>
-	<h1>เรียกข้อมูลจาก API bis42_get_member</h1>
-	<h3>รหัสประจำตัว xxxxx</h3>
-	<h3>ชื่อ xxxx</h3>
-	<h3>กลุ่มเรียน xxxxx</h3>
-    <button type="button">เรียกดูข้อมูล</button>
+    <h1>เรียกข้อมูลจาก API bis41_get_car</h1>
+	<h3>รหัสประจำตัว 4430202551-9</h3>
+	<h3>นายศราวุธ ซื่อวุฒิกุล</h3>
+	<h3>กลุ่มเรียน BIS4N1</h3>
+    <select id="filter">
+        <option value="HONDA">HONDA</option>
+        <option value="TOYOTA">TOYOTA</option>
+    </select>
+    <button type="button" onclick="bisgetCar()">เรียกดูข้อมูล</button>
 </body>
 </html>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script type=text/javascript>
-	
+    function bisgetCar(){
+        let keysearch = document.getElementById("filter").value;
+        let request_data = {
+            "keysearch":keysearch
+        }
+        let uri = "http://localhost:8080/project/api/bis41_get_car.php";
+        $.ajax({
+            type:"POST",
+            url:uri,
+            async:false,
+            data:JSON.stringify(request_data),
+            success:function(response){
+                console.log(response.datalist);
+            },error:function(error){
+                console.log(error);
+            }
+        });        
+    }
 </script>
